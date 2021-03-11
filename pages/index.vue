19aef8820bd83c2164de663d65f849fa59c543f7
Your Name <you@example.com>
2021-03-12 00:56:30 +0900

    vue-share-itの動作用に作成

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template lang="javascript">
+<div class="container">
+    <!-- <share-it /> -->
+    <!-- <share-it dark />
+    <share-it dense />
+    <share-it outline /> -->
+    <share-it :targets="['twitter', 'facebook']" />
+    <share-it icons />
+</div>
+</template>
+ 
+<script>
+export default {
+    components: {
+ 
+    },
+    data() {
+        return {}
+    }
+}
+</script>
+ 
+<style>
+.container {
+    margin: 0 auto;
+    min-height: 80vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 20px;
+}
+</style>
---
+++ plugins/plugin.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import shareIt from 'vue-share-it';
+ 
+Vue.use(shareIt);
